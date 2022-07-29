607943e953cbdcd16cc889e4242cd8d3e4989c02
Javier Mendiola <javier362@revature.net>
2022-07-29 18:00:22 -0400

    Almost finished

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -10,9 +12,20 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductDetailsComponent implements OnInit {
 
   product: Array<any> = []
-
+  cartCount!: number;
+  products: {
+    product: Product,
+    quantity: number
+  }[] = [];
+  subscription!: Subscription;
+  totalPrice: number = 0;
+  selectedId!: number;
+    Product! : Product;
+    productId!: number;
+  
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private productService: ProductService
   ) { }
 
@@ -30,6 +43,62 @@ export class ProductDetailsComponent implements OnInit {
       }
     })
     console.log(this.product)
+    this.subscription = this.productService.getCart().subscribe(
+      (cart) => {
+        this.cartCount = cart.cartCount;
+        this.products = cart.products;
+        this.totalPrice = cart.totalPrice;
+      }
+    );
+    //const productId: any = this.route.snapshot.paramMap.get('id') ;
+    this.route.paramMap.subscribe((params: ParamMap) => {
+
+      this.productId = Number (params.get('id'));
+    })
+    console.log(this.productId)
+    this.productService.getSingleProduct(this.productId).subscribe( 
+      (resp: Product) => this.Product = resp,
+      (err) => console.log(err),
+      () => console.log("Product retrieved successfully")
+      )
+  };
+  addToCart(product: Product): void {
+
+    let inCart = false;
+
+    this.products.forEach(
+      (element) => {
+        if(element.product == product){
+          ++element.quantity;
+          let cart = {
+            cartCount: this.cartCount + 1,
+            products: this.products,
+            totalPrice: this.totalPrice + product.price
+          };
+          this.productService.setCart(cart);
+          inCart=true;
+          return;
+        };
+      }
+    );
+
+    if(inCart == false){
+      let newProduct = {
+        product: product,
+        quantity: 1
+      };
+      this.products.push(newProduct);
+      let cart = {
+        cartCount: this.cartCount + 1,
+        products: this.products,
+        totalPrice: this.totalPrice + product.price
+      }
+      this.productService.setCart(cart);
+    }
+      
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
